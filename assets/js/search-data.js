1439ddcbcb3cd2a0d856a95193eaac7a1d5fd998
yusd24 <69486535+yusd24@users.noreply.github.com>
2025-02-04 15:54:47 +0000

    Deploying to gh-pages from @ yusd24/yusd24.github.io@b0882d6240b85f27c2aadeb90d6cebedb14eeae5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-curriculum-vitae",
           title: "Curriculum Vitae",
           description: "A PDF version of my curriculum vitae is also available.",
