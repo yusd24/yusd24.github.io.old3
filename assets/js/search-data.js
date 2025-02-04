969e1a42323a772a69f566b7ba95a71c3a3366a4
yusd24 <69486535+yusd24@users.noreply.github.com>
2025-02-04 15:44:05 +0000

    Deploying to gh-pages from @ yusd24/yusd24.github.io@1e9a781c256be71a037664150116b28bd4cc7f56 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,9 +37,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+        },{id: "nav-curriculum-vitae",
+          title: "Curriculum Vitae",
+          description: "A PDF version of my curriculum vitae is also available.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
