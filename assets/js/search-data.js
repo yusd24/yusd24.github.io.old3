fecf1c3fe29ddc8e1de9f9760129c7f7f0687f73
yusd24 <69486535+yusd24@users.noreply.github.com>
2025-02-04 16:10:41 +0000

    Deploying to gh-pages from @ yusd24/yusd24.github.io@26695d5567dba2d344516d409b27aacd2f99d20d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,7 +28,7 @@ ninja.data = [{
           description: "Materials for courses you taught. Replace this text with your description.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/teaching/";
+            window.location.href = "/projects/";
           },
         },{id: "dropdown-publications",
               title: "publications",
