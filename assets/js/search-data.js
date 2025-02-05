8f319aaaa98610d01afc75d9f583be3977842af6
yusd24 <69486535+yusd24@users.noreply.github.com>
2025-02-05 12:29:32 +0000

    Deploying to gh-pages from @ yusd24/yusd24.github.io@bd586eb514720c75ab1af5d6b19a0705fa915baf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -513,6 +513,13 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
+        id: 'social-custom_social',
+        title: 'Custom_social',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.alberteinstein.com/", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
